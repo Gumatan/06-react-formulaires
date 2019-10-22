17ed88a787f9f1a79c1e234a39763a8030fc1480
Gumatan <arnaut.demaret@gmail.com>
2019-10-22 09:28:04 +0200

    fetching and displaying  employees from the simpson quote API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DisplayEmployee from "./components/DisplayEmployee";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: ""
+    };
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+
+  getEmployee() {
+    axios
+      .get("https://quests.wilders.dev/simpsons-quotes/quotes")
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        console.log(data[0]);
+
+        this.setState({
+          employee: data[0]
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>
+          Get employee
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
